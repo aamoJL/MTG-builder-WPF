86b4bb6891de5099c8c679c13bd6da855fe086da
Joni Laukka <jonilaukka@hotmail.com>
2021-06-19 15:40:44 +0300

    Merged with old project

---
+++ DeckBuilding.xaml.cs
@@ -0,0 +1,360 @@
+﻿using Microsoft.Win32;
+using MTG.Scryfall;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace MTG
+{
+    /// <summary>
+    /// Interaction logic for DeckBuilding.xaml
+    /// </summary>
+    public partial class DeckBuilding : Window
+    {
+        string collectionsPath = "Resources/Collections/";
+        string selectedCollectionPath = "";
+        readonly CardCollection cardCollection = new CardCollection();
+
+        public DeckBuilding()
+        {
+            InitializeComponent();
+
+            Directory.CreateDirectory(collectionsPath);
+
+            CollectionListBox.ItemsSource = cardCollection.Cards;
+
+            // Get card sets from a file and add them to combobox
+            CardSetComboBox.ItemsSource = GetCardSets();
+
+            // Get collection names from a file and add them to combobox
+            CardCollectionComboBox.ItemsSource = GetCollectionNames();
+        }
+
+        private List<CardSet> GetCardSets()
+        {
+            try
+            {
+                string path = "Resources/Scryfall_sets.json";
+                string json = File.ReadAllText(path);
+
+                CardSetData setBase = JsonConvert.DeserializeObject<CardSetData>(json);
+                return setBase.data;
+            }
+            catch (IOException e)
+            {
+                Debug.WriteLine(e.Message);
+                throw;
+            }
+        }
+        private string[] GetCollectionNames()
+        {
+            Directory.CreateDirectory(collectionsPath);
+
+            string[] files = Directory.GetFiles(collectionsPath, "*.json");
+            string[] fileNames = new string[files.Length];
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                fileNames[i] = System.IO.Path.GetFileNameWithoutExtension(files[i]);
+            }
+
+            return fileNames;
+        }
+        private List<Card> GetCardsFromCollection(CardCollection collection)
+        {
+            List<Card> cards = new List<Card>();
+            foreach (CollectionCard collectionCard in collection.Cards)
+            {
+                cards.Add(collectionCard.Card);
+            }
+
+            return cards;
+        }
+        private List<Card> GetCardsFromCollection(List<CollectionCard> collectionCards)
+        {
+            List<Card> cards = new List<Card>();
+            foreach (CollectionCard collectionCard in collectionCards)
+            {
+                cards.Add(collectionCard.Card);
+            }
+            return cards;
+        }
+        private List<Card> FetchScryfallSetCards(string searchUri)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(searchUri);
+                return JsonConvert.DeserializeObject<CardData>(json).data;
+            }
+        }
+
+        private void AddCardToCollection(Card card)
+        {
+            cardCollection.AddCard(card);
+        }
+        private void AddCardToCollection(CollectionCard card)
+        {
+            cardCollection.AddCard(card);
+        }
+        private void RemoveCardFromCollection(CollectionCard card)
+        {
+            cardCollection.RemoveCard(card);
+        }
+
+        private void SaveCollection(string path)
+        {
+            if (cardCollection.Name == "")
+            {
+                // Create new collection if collections has not been selected
+                CopyCollection();
+            }
+            else
+            {
+                cardCollection.Save(path);
+            }
+        }
+        private List<CollectionCard> ReadCollectionFromFile(string path)
+        {
+            using (StreamReader file = File.OpenText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                return (List<CollectionCard>)serializer.Deserialize(file, typeof(List<CollectionCard>));
+            }
+        }
+
+        private void ChangeCollection(string path)
+        {
+            List<CollectionCard> cards = ReadCollectionFromFile(path);
+            string collectionsName = System.IO.Path.GetFileNameWithoutExtension(path);
+
+            cardCollection.ChangeCollection(cards, collectionsName);
+            CollectionTextBlock.Text = collectionsName;
+            selectedCollectionPath = path;
+        }
+
+        private void CardSetComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(CardSetComboBox.SelectedIndex == -1) { return; }
+            CardSet set = CardSetComboBox.SelectedItem as CardSet;
+
+            List<Card> cards = FetchScryfallSetCards(set.search_uri);
+            CardSetImageListBox.ItemsSource = cards;
+            CardCollectionComboBox.SelectedIndex = -1;
+        }
+        private void CardCollectionComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(CardCollectionComboBox.SelectedIndex == -1) { return; }
+            string collectionName = CardCollectionComboBox.SelectedItem.ToString();
+
+            List<CollectionCard> collectionCards = ReadCollectionFromFile($"{collectionsPath}{collectionName}.json");
+
+            if(collectionCards != null)
+            {
+                List<Card> cards = GetCardsFromCollection(collectionCards);
+                CardSetImageListBox.ItemsSource = cards;
+                CardSetComboBox.SelectedIndex = -1;
+            }
+            else
+            {
+                CardSetImageListBox.ItemsSource = null;
+            }
+        }
+
+        private void CardImage_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton != MouseButton.Left) { return; }
+            
+            //Only left double click
+            Card card = CardSetImageListBox.SelectedItem as Card;
+
+            if (card != null)
+            {
+                AddCardToCollection(card);
+            }
+        }
+        private void CardImage_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Image img = sender as Image;
+            Card card = CardSetImageListBox.SelectedItem as Card;
+
+            if (img != null && card != null)
+            {
+                if (card.HasTwoFaces)
+                {
+                    string currentSource = img.Source.ToString();
+                    if(card.card_faces[0].image_uris["normal"] == currentSource)
+                    {
+                        img.Source = card.SecondaryFace;
+                    }
+                    else
+                    {
+                        img.Source = card.PrimaryFace;
+                    }
+                }
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionCard card = CollectionListBox.SelectedItem as CollectionCard;
+
+            if (card != null)
+            {
+                AddCardToCollection(card);
+            }
+        }
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionCard card = CollectionListBox.SelectedItem as CollectionCard;
+
+            if (card != null)
+            {
+                RemoveCardFromCollection(card);
+            }
+        }
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveCollection(selectedCollectionPath);
+        }
+        private void SaveAsButton_Click(object sender, RoutedEventArgs e)
+        {
+            CopyCollection();
+        }
+
+        private void OpenCollectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (cardCollection.UnsavedChanges)
+            {
+                MessageBoxResult message = UnsavedChangesDialog();
+                switch (message)
+                {
+                    case MessageBoxResult.Cancel:
+                        return;
+                    case MessageBoxResult.Yes:
+                        SaveCollection(selectedCollectionPath);
+                        break;
+                    case MessageBoxResult.No:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.json)|*.json|All files (*.*)|*.*";
+            string CombinedPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), collectionsPath);
+            openFileDialog.InitialDirectory = System.IO.Path.GetFullPath(CombinedPath);
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ChangeCollection(openFileDialog.FileName);
+            }
+        }
+
+        private void NewCollectionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (cardCollection.UnsavedChanges)
+            {
+                MessageBoxResult message = UnsavedChangesDialog();
+                switch (message)
+                {
+                    case MessageBoxResult.Cancel:
+                        return;
+                    case MessageBoxResult.Yes:
+                        SaveCollection(selectedCollectionPath);
+                        break;
+                    case MessageBoxResult.No:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            CreateNewCollection();
+        }
+
+        private void CreateNewCollection()
+        {
+            // Configure save file dialog box
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.FileName = "NewCollection"; // Default file name
+            dialog.DefaultExt = ".json"; // Default file extension
+            dialog.InitialDirectory = Path.GetFullPath(collectionsPath);
+            dialog.Filter = "Text documents (.json)|*.json"; // Filter files by extension
+
+            // Show save file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string path = dialog.FileName;
+
+                cardCollection.Clear();
+                cardCollection.Name = dialog.SafeFileName;
+                SaveCollection(path);
+                ChangeCollection(path);
+            }
+        }
+        private void CopyCollection()
+        {
+            // Configure save file dialog box
+            var dialog = new SaveFileDialog();
+            dialog.FileName = "CopiedCollection"; // Default file name
+            dialog.DefaultExt = ".json"; // Default file extension
+            dialog.InitialDirectory = Path.GetFullPath(collectionsPath);
+            dialog.Filter = "Text documents (.json)|*.json"; // Filter files by extension
+
+            // Show save file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string path = dialog.FileName;
+
+                cardCollection.Name = dialog.SafeFileName;
+                SaveCollection(path);
+                ChangeCollection(path);
+            }
+        }
+
+        private MessageBoxResult UnsavedChangesDialog()
+        {
+            // Ask if user wants to save last collection
+            string messageBoxText = "Do you want to save changes?";
+            string caption = "Save?";
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result;
+
+            result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+
+            return result;
+        }
+
+        private void SetCollectionTab_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CollectionTab.IsSelected)
+            {
+                CardCollectionComboBox.ItemsSource = GetCollectionNames();
+            }
+        }
+    }
+}
---
+++ Scryfall/Scryfall.cs
@@ -0,0 +1,254 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace MTG.Scryfall
+{
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public class Card
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public Dictionary<string, string> image_uris { get; set; }
+        public string image_status { get; set; }
+        public int cardmarket_id { get; set; }
+        public List<CardFace> card_faces { get; set; }
+        public decimal cmc { get; set; }
+        public List<CardColors> color_identity { get; set; }
+        public List<string> keywords { get; set; }
+        public string type_line { get; set; }
+        public CardRarity rarity { get; set; }
+        public string set { get; set; }
+
+        public BitmapImage PrimaryFace
+        {
+            get
+            {
+                if (image_uris == null)
+                {
+                    if (card_faces.Count > 1 && card_faces[0].image_uris != null)
+                    {
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        bitmapImage.UriSource = new Uri(card_faces[0].image_uris["normal"]);
+                        bitmapImage.EndInit();
+                        return bitmapImage;
+                    }
+
+                    return null;
+                }
+                else
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.UriSource = new Uri(image_uris["normal"]);
+                    bitmapImage.EndInit();
+                    return bitmapImage;
+                }
+            }
+        }
+        public BitmapImage SecondaryFace
+        {
+            get
+            {
+                if (!HasTwoFaces) { return null; }
+                else
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.UriSource = new Uri(card_faces[1].image_uris["normal"]);
+                    bitmapImage.EndInit();
+                    return bitmapImage;
+                }
+            }
+        }
+        public bool HasTwoFaces => card_faces != null;
+    }
+    public class CardSet
+    {
+        public string name { get; set; }
+        public string search_uri { get; set; }
+    }
+
+    public class CollectionCard
+    {
+        public virtual Card Card { get; set; }
+        public virtual int Count { get; set; }
+
+        public CollectionCard(Card card, int count = 1)
+        {
+            Card = card;
+            Count = count;
+        }
+    }
+    public class CardCollection
+    {
+        private readonly ObservableCollection<ListBoxCollectionCard> _cards;
+
+        public string Name { get; set; }
+        public bool UnsavedChanges { get; set; }
+        public ObservableCollection<ListBoxCollectionCard> Cards { get { return _cards; } }
+
+        public CardCollection()
+        {
+            _cards = new ObservableCollection<ListBoxCollectionCard>();
+            Name = "";
+        }
+
+        public void Clear()
+        {
+            UnsavedChanges = true;
+            Cards.Clear();
+        }
+
+        public void AddCard(Card card)
+        {
+            foreach (CollectionCard collectionCard in Cards)
+            {
+                if (collectionCard.Card.id == card.id)
+                {
+                    collectionCard.Count++;
+                    UnsavedChanges = true;
+                    return;
+                }
+            }
+
+            Cards.Add(new ListBoxCollectionCard(card));
+            UnsavedChanges = true;
+        }
+        public void AddCard(CollectionCard card)
+        {
+            foreach (CollectionCard collectionCard in Cards)
+            {
+                if (collectionCard.Card.id == card.Card.id)
+                {
+                    collectionCard.Count++;
+                    UnsavedChanges = true;
+                    return;
+                }
+            }
+
+            Cards.Add(new ListBoxCollectionCard(card));
+            UnsavedChanges = true;
+        }
+
+        public void RemoveCard(CollectionCard card)
+        {
+            for (int i = 0; i < Cards.Count; i++)
+            {
+                CollectionCard collectionCard = Cards[i];
+
+                if (collectionCard.Card.id == card.Card.id)
+                {
+                    collectionCard.Count--;
+                    if (collectionCard.Count <= 0)
+                    {
+                        Cards.RemoveAt(i);
+                    }
+                    UnsavedChanges = true;
+                    break;
+                }
+            }
+        }
+
+        public void Save(string path)
+        {
+            if (Name == "" || path == "") { return; }
+            SaveCollectionToFile(path);
+            UnsavedChanges = false;
+        }
+
+        public void ChangeCollection(List<CollectionCard> cards, string name)
+        {
+            Name = name;
+            Cards.Clear();
+            for (int i = 0; i < cards.Count; i++)
+            {
+                _cards.Add(new ListBoxCollectionCard(cards[i]));
+            }
+
+            UnsavedChanges = false;
+        }
+
+        private void SaveCollectionToFile(string path)
+        {
+            using (StreamWriter file = File.CreateText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, Cards);
+            }
+        }
+    }
+
+    public class CardData
+    {
+        public List<Card> data { get; set; }
+
+        public string asd()
+        {
+            return "asd";
+        }
+    }
+    public class CardSetData
+    {
+        public List<CardSet> data { get; set; }
+    }
+
+    public class ListBoxCollectionCard : CollectionCard, INotifyPropertyChanged
+    {
+        public ListBoxCollectionCard(Card card, int count = 1) : base(card, count)
+        {
+        }
+
+        public ListBoxCollectionCard(CollectionCard card) : base(card.Card, card.Count)
+        {
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return base.Count;
+            }
+            set
+            {
+                base.Count = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class CardFace
+    {
+        public List<CardColors> colors { get; set; }
+        public Dictionary<string, string> image_uris { get; set; }
+        public string name { get; set; }
+        public string type_line { get; set; }
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum CardColors
+    {
+        W, U, B, R, G
+    }
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum CardRarity
+    {
+        common, uncommon, rare, special, mythic, bonus
+    }
+}
